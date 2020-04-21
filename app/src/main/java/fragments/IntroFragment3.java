d0556817ecc5ad6b2d3578818e907ec13478c7cf
Ibrahimcodes <ibrahimcodes@gmail.com>
2020-04-21 08:10:19 +0100

    Did the layout for intro screens

---
+++ app/src/main/java/fragments/IntroFragment3.java
@@ -0,0 +1,34 @@
+package fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.snappmi.covid19_ng.R;
+
+public class IntroFragment3 extends Fragment {
+
+    public IntroFragment3() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_intro_fragment_3, container, false);
+    }
+
+}
